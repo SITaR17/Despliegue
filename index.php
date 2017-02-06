031de9c0f04007dff3255d9ea6b444e71b15e048
Itzi <susana.tuya@gmail.com>
2017-02-06 20:23:02 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prueba de Despliegue desde Github</title>
+</head>
+<body>
+<h1>Despliegue desde Github</h1>
+<?php
+	phpinfo();
+?>
+</body>
+</html>
